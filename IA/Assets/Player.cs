909a68f8198ef84d69ec311a83c2a6f3f38063d8
LorenzoMarnat <lmarnat14@gmail.com>
2020-09-10 16:02:39 +0200

    Move Player

---
+++ IA/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int force = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
+        float y = Input.GetAxis("Vertical") * Time.fixedDeltaTime;
+
+        Vector2 movement = new Vector2(x*force, y*force);
+
+        // Appelle la fonction AddForce() du Rigidbody2D rb2d avec le mouvement multiplié par la vitesse.
+        rb.transform.Translate(movement);
+    }
+}
